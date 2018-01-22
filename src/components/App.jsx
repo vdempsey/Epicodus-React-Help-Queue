cdd8423776b48aa627694aa4a1cbcc44953a604b
Susannah Lowe and Valia Dempsey <susannah+valia@>
2018-01-22 08:47:27 -0800

    Add constructor to NewTicketControl component, replace NewTicketForm with NewTicketControl in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
 
 function App(){
   return (
@@ -10,7 +11,8 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
+        <Route component={Error404} />
       </Switch>
     </div>
   );
--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 class NewTicketControl extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
   render() {
     return (
       <div>
