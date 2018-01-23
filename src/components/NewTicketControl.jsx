a4c714b9b16cf1e8aaceb77c30bdc5574323d660
Valia Dempsey <valentinaml@gmail.com>
2018-01-22 23:29:32 -0800

    Remove code entered to display errors

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -8,8 +8,7 @@ class NewTicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
-      testState: 'hey!'
+      formVisibleOnPage: false
     };
     this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
   }
